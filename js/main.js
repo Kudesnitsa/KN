d058b0686d947369d6d095189cdecca951c09c6c
Kudesnitsa <diana199725@gmail.com>
2018-02-14 13:46:59 +0200

    add mobile menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
 jQuery(document).ready(function () {
+    jQuery('.mobil_menu .chosen_language').click(function () {
+        jQuery('.mobil_language').toggle();
+    });
+    jQuery('.close').click(function () {
+        jQuery('.mobil_language').css("display","none");
+        jQuery('.mobil_category').css("display","none");
+
+    });
+    jQuery('.mobile .show_menu').click(function () {
+        jQuery('.black_fond').toggle();
+        jQuery('.mobil_menu').toggle();
+    });
+    jQuery('.mobil_menu .have_sub_menu').click(function () {
+        jQuery('.mobil_category').toggle();
+    });
+
+
+
+    jQuery('.black_fond').click(function () {
+        jQuery('.black_fond').css("display","none");
+        jQuery('.mobil_menu').css("display","none");
+        jQuery('.mobil_language').css("display","none");
+        jQuery('.mobil_category').css("display","none");
+    });
 
     var img_height_1 = jQuery('.exhibition_img_1 img').height();
     var img_height_2 = jQuery('.exhibition_img_2 img').height();
     jQuery('.exhibition_img').height(img_height_1 > img_height_2 ? img_height_2 : img_height_1);
-    jQuery('.chosen_language').click(function () {
+    jQuery('.sub_nav .chosen_language').click(function () {
         jQuery('.language').toggleClass("decor_language");
         jQuery('.selector_language').toggle();
+
 
 
     });
@@ -15,8 +40,8 @@ jQuery(document).ready(function () {
         var lang = chosen_language.html();
         chosen_language.html(jQuery(this).html());
         jQuery(this).html(lang);
-
-        jQuery('.selector_language').toggle();
+        jQuery('.sub_nav .selector_language').toggle();
+        jQuery('.mobil_language').css("display","none");
 
     });
 
@@ -37,6 +62,7 @@ jQuery(document).ready(function () {
         jQuery('.persona_office_block').toggle();
 
     });
+
 
 
 });
